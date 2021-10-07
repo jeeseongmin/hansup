59127cd216dfdc8ab857de941fc60721eabfee63
dpcks0919 <21600120@handong.edu>
2021-10-07 16:20:17 +0900

    yc - homePage 작업

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'hansupBrown': '#6C4D3F',
+				'homePink': '#FCF4ED',
+				'homeGray': '#F1F0EC',
+			},
+			width: {
+			    '3/10': '30%',
+			
+			}
+		},
 	},
 	variants: {
 		extend: {},
