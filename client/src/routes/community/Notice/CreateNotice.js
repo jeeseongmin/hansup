d0cd2dd7cb817787d1212445df4e05e0b277c454
21500706 <21500706@handong.edu>
2021-10-22 00:38:17 +0900

    update notice

--- client/src/routes/community/Notice/CreateNotice.js
+++ client/src/routes/community/Notice/CreateNotice.js
@@ -11,17 +11,21 @@ const CreateNotice = () => {
 	const [includeImg, setIncludeImg] = useState(false);
 	const history = useHistory();
 	const contentRef = useRef(null);
-	const emailRef = useRef(null);
+	const titleRef = useRef(null);
 	const passwordRef = useRef(null);
+	const [isImageUpload, setIsImageUpload] = useState(true);
 
 	const onClickCheckBox = (e) => {
 		setIncludeImg(e.target.checked);
+		// 이미지 포함할 때 (true) image Upload가 된 상태가 마찬가지
+		setIsImageUpload(includeImg);
+		const cp = { ...info };
+		cp["imgList"] = [];
+		setInfo(cp);
 	};
 	const [info, setInfo] = useState({
+		title: "",
 		content: "",
-		email: "",
-		password: "",
-		response: "",
 		imgList: [],
 	});
 	const changeInfo = (e, type) => {
@@ -37,29 +41,22 @@ const CreateNotice = () => {
 	};
 
 	const submit = () => {
-		if (info.content === "") {
+		if (info.title === "") {
+			alert("제목을 입력해주세요!");
+			titleRef.current.focus();
+		} else if (info.content === "") {
 			alert("내용을 입력해주세요!");
 			contentRef.current.focus();
-		} else if (info.email === "") {
-			alert("이메일을 입력해주세요!");
-			emailRef.current.focus();
-		} else if (info.password === "") {
-			alert("패스워드를 입력해주세요!");
-			passwordRef.current.focus();
-		} else if (info.imgList.length === 0) {
+		} else if (includeImg && info.imgList.length === 0) {
 			alert("1개 이상의 이미지를 업로드 해주세요.");
 		} else {
-			console.log("info", info);
-
 			axios
 				.post(
-					"/api/review/add",
+					"/api/notice/add",
 					{
 						key: process.env.REACT_APP_API_KEY,
+						title: info.title,
 						content: info.content,
-						email: info.email,
-						password: info.password,
-						response: info.response,
 						imgList: info.imgList,
 					},
 					{
@@ -71,7 +68,7 @@ const CreateNotice = () => {
 				)
 				.then((response) => {
 					alert("업로드 되었습니다.");
-					history.push("/community/review/list");
+					history.push("/community/notice/list");
 					document.getElementById("scrollRef").scrollTo(0, 0);
 				})
 				.catch((response) => {
@@ -99,19 +96,20 @@ const CreateNotice = () => {
 				</div>
 			</div>
 			<NoticeFormBlock
-				contentRef={contentRef}
-				emailRef={emailRef}
-				passwordRef={passwordRef}
 				changeInfo={changeInfo}
+				titleRef={titleRef}
+				contentRef={contentRef}
 				info={info}
 				isEdit={false}
 				includeImg={includeImg}
+				setIsImageUpload={setIsImageUpload}
+				isImageUpload={isImageUpload}
 			/>
 
 			<div class="flex justify-between items-center flex-col md:flex-row">
 				<Link
 					class="mb-4 md:mb-0 w-full md:w-auto  cursor-pointer px-0 md:px-16 py-2 justify-center border border-hansupBrown text-hansupBrown flex flex-row items-center hover:bg-hansupBrown hover:text-white hover:font-bold"
-					to={"/community/review/list"}
+					to={"/community/notice/list"}
 					onClick={() => window.scrollTo(0, 0)}
 				>
 					뒤로 가기
