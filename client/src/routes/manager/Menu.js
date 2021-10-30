9abc01c654eed192d65d8f1563db1efd7a8f1a60
21500706 <21500706@handong.edu>
2021-10-30 19:59:10 +0900

    update title

--- client/src/routes/manager/Menu.js
+++ client/src/routes/manager/Menu.js
@@ -5,7 +5,7 @@ import ContentLayout from "components/Layout/ContentLayout";
 const Menu = () => {
 	return (
 		<PageLayout>
-			<ContentLayout subtitle={"메뉴관리"}>
+			<ContentLayout subtitle={"메뉴 추가"}>
 				<div class="flex flex-col">
 					<p class="text-hansupBrown text-lg mb-8 font-semibold">
 						인당 15,000원 (케이터링 주문 최소 인원 : 10인)
